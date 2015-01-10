5e878c422af5e595a00c024ef3fb3b06d2f346a9
enikey <enikey87@gmail.com>
2015-01-10 12:03:01 +0600

    Add selection to current gun & sound. Interrupt reload if gun was changed.

--- Assets/Scripts/UI/ChangeWeaponOnClick.cs
+++ Assets/Scripts/UI/ChangeWeaponOnClick.cs
@@ -2,6 +2,15 @@
 using System.Collections;
 
 public class ChangeWeaponOnClick : MonoBehaviour {
+    public AudioClip sound;
+
+    Transform highlight;
+
+    void Awake()
+    {
+        highlight = transform.FindChild("Highlight");
+    }
+
     public void OnClick()
     {
         GameObject hunter = GameObject.Find("Guns");
@@ -11,5 +20,16 @@ public class ChangeWeaponOnClick : MonoBehaviour {
             Transform child = hunter.transform.GetChild(i);
             child.gameObject.SetActive(child.gameObject.name.Equals(gameObject.name));
         }
+
+        Transform parent = transform.parent;
+
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            ChangeWeaponOnClick buttonScript = parent.GetChild(i).GetComponent<ChangeWeaponOnClick>();
+            if (buttonScript == null) continue;
+            buttonScript.highlight.gameObject.SetActive(buttonScript == this);
+        }
+
+        if (sound != null) AudioCenter.PlayClipAtMainCamera(sound);
     }
 }
